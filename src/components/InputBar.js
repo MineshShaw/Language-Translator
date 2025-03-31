fbe997407e8aa2ba1a46d0bdaaaf48b05985c420
MineshShaw <mineshshaw2007@gmail.com>
2025-03-31 16:46:41 +0530

    styling changes

--- src/components/InputBar.js
+++ src/components/InputBar.js
@@ -25,52 +25,63 @@ const InputBar = () => {
 
   return (
     <>
-      <div className="input-bar flex flex-col items-center justify-center mb-4">
-        <input
-          className="input rounded-full px-8 py-3 border-2 border-transparent focus:outline-none focus:border-blue-500 placeholder-gray-400 transition-all duration-300 shadow-md"
-          placeholder="Enter text to translate..."
-          required
-          type="text"
-          value={inputText}
-          onChange={handleInputChange}
-        />
-        <label
-          htmlFor="sourceLang"
-          className="block text-lg font-medium text-gray-700"
-        >
-          Source Language:
-        </label>
-        <select
-          id="sourceLang"
-          value={sourceLang}
-          onChange={handleSourceChange}
-          className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-base"
-        >
-          {languages.map((lang) => (
-            <option key={lang.code} value={lang.code}>
-              {lang.name}
-            </option>
-          ))}
-        </select>
-
-        <label
-          htmlFor="targetLang"
-          className="block text-lg font-medium text-gray-700"
-        >
-          Target Language:
-        </label>
-        <select
-          id="targetLang"
-          value={targetLang}
-          onChange={handleTargetChange}
-          className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-base"
-        >
-          {languages.map((lang) => (
-            <option key={lang.code} value={lang.code}>
-              {lang.name}
-            </option>
-          ))}
-        </select>
+      <div className="input-bar flex gap-3 flex-col items-center justify-center mb-4">
+        <div className="w-full flex flex-col items-center justify-center mb-4">
+          <label
+            htmlFor="inputText"
+            className="block text-lg font-medium text-gray-700"
+          >
+            Input:
+          </label>
+          <input
+            className="input rounded-full px-8 py-3 border-2 border-transparent focus:outline-none focus:border-blue-500 placeholder-gray-400 transition-all duration-300 shadow-md"
+            placeholder="Enter text to translate..."
+            required
+            type="text"
+            value={inputText}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="sourceLang"
+            className="block text-lg font-medium text-gray-700"
+          >
+            Source Language:
+          </label>
+          <select
+            id="sourceLang"
+            value={sourceLang}
+            onChange={handleSourceChange}
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-base"
+          >
+            {languages.map((lang) => (
+              <option key={lang.code} value={lang.code}>
+                {lang.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label
+            htmlFor="targetLang"
+            className="block text-lg font-medium text-gray-700"
+          >
+            Target Language:
+          </label>
+          <select
+            id="targetLang"
+            value={targetLang}
+            onChange={handleTargetChange}
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-base"
+          >
+            {languages.map((lang) => (
+              <option key={lang.code} value={lang.code}>
+                {lang.name}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,9 @@ const Home = () => {
     <LanguageProvider>
       <div className="relative h-screen w-screen flex items-center justify-center">
         <Background />
-        <div className="z-10 border-2 p-4 rounded-lg backdrop-blur-md flex flex-col items-center justify-center">
-        <h1 className="text-2xl font-bold mb-4">Language Translator</h1>
-          <div className="container">
+        <div className="z-10 flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-bold font-serif mb-4">Language Translator</h1>
+          <div className="flex flex-row items-center justify-evenly border-2 p-[20px] rounded-lg w-[100%] backdrop-blur-md">
             <div className="left flex flex-col items-center justify-center mb-4">
               <InputBar />
               <TranslateButton />
